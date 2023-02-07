3f343f893a8d6a413addf0c18b5a1155e05c6a8d
statik2002 <statik2002@mail.ru>
2023-02-07 08:49:55 +0800

    Правки
    1. Убрал инлайн стиль
    2. Убрал % из размеров блоков внутри flex контейнера
    3. Удалил неиспользуемый класс стиля
    4. Убрал стиль из тегов в класс
    5. Зафиксировал размер кнопок
    6. Выровнял элементы контейнера по центру

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from livereload import Server, shell
 
 server = Server()
-server.watch('*.html', shell('make html', cwd='docs'))
+server.watch('*.html', shell('make html', cwd=''))
 server.serve(root='')
